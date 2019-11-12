d30799403597e42dd0a766ef1d7cb0b43e8cd6f2
Christian Cruz <cruzchristian@outlook.com>
2019-11-12 00:13:41 -0500

    fix removal leading zeros on non numeric key

--- app/controllers/spree/api/v1/variants_controller_decorator.rb
+++ app/controllers/spree/api/v1/variants_controller_decorator.rb
@@ -5,8 +5,9 @@ Spree::Api::V1::VariantsController.class_eval do
   private
 
   def prepare_for_product_name_or_sku_search
-    if params[:q].has_key?(:product_name_or_sku_cont)
-      params[:q][:product_name_or_sku_cont] = params[:q][:product_name_or_sku_cont].sub!(/^0+/, "")
+    if params[:q].has_key?(:product_name_or_sku_cont) && 
+      !!(params[:q][:product_name_or_sku_cont] =~ /\A[-+]?[0-9]+\z/) #if it is numeric
+      params[:q][:product_name_or_sku_cont] = params[:q][:product_name_or_sku_cont].to_i.to_s
     end
   end
 
